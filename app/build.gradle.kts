da583db50a693ed2869ba54d2921c2bcb9d08b12
Dorian <gironpratodorian@gmail.com>
2026-09-07 15:11:01 -0500

    chore: reorganizar paquetes y habilitar viewBinding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -39,4 +42,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
 }
