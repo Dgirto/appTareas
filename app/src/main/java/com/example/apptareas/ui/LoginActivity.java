fb08fe3f03d8be903b9ef4c36c337a5c92fbae68
Dorian <gironpratodorian@gmail.com>
2026-09-07 16:24:06 -0500

    feat: declarar Activities en el manifest y crear clases base

---
+++ app/src/main/java/com/example/apptareas/ui/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.apptareas.ui;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.apptareas.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
